f63e2037b35445052266303e2d4a02fe74ce84a4
ollikkom <olyaamh@mail.ru>
2017-05-13 01:08:38 +0300

    yrra

--- main.cpp
+++ main.cpp
@@ -1,35 +1,43 @@
 #include <iostream>
 #include <vector>
 
-
 template<class It, class Cmp>
-void qsort(It beg, It end, Cmp cmp){
-    if (beg != end){
-        It ffirst = beg;
-        It fend = end;
-        It cen = beg+1;
-        while (cen != fend) {
+void qsort(It beg, It end, Cmp cmp) {
+    if (beg != end) {
+        It left = beg;
+        It right = end;
+        It tmp = beg;
+        left++;
+        while (left != right) {
 
-            if (cmp(*cen, *ffirst)) {
-                ++cen;
-            } else {
-                while ((cen != fend) && cmp(*ffirst, *fend)) {
-                    --fend;
+            if (cmp(*left, *tmp))
+                ++left;
+
+            else {
+                --right;
+                while ((left != right) && cmp(*tmp, *right))
+                    --right;
+                if (left != right) {
+                    std::swap(*left, *right);
                 }
-                std::swap(*cen, *fend);
             }
-        }
-        --cen;
-        std::swap(*ffirst, *cen);
-        qsort(beg, cen, cmp);
-        qsort(fend, end, cmp);
 
+        }
+        --left;
+        std::swap( *tmp, *left );
+        qsort(beg, left, cmp);
+        qsort(right, end, cmp);
     }
 }
 
 
-int main() {
-    std::cout<< "Sort" << std::endl;
+int main()
+{
+    std::vector<int> a = { 5, 12, 13, 0, 35, 3, 1, 4 };
+    qsort<std::vector<int>::iterator, std::less<int>>(a.begin(), a.end(), std::less<int>());
+    for (auto it = a.begin(); it != a.end(); ++it)
+    {
+        std::cout << *it << " ";
+    }
     return 0;
-
 }
